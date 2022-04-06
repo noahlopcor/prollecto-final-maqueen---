8829fa169397ab2f27af56cb6865a5c16b3b45c4
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-04-06 09:30:48 +0200

    nnnn

--- main.ts
+++ main.ts
@@ -21,3 +21,27 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 radio.setGroup(5)
+music.playMelody("C5 B A G F E D C ", 120)
+basic.showString("hola")
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 6) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        radio.sendNumber(0)
+    }
+})
+basic.forever(function () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
